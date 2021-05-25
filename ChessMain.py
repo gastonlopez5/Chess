3504e7106c157b7cef2c8f4252d58584e3666190
gastonlopez5 <43733902+gastonlopez5@users.noreply.github.com>
2021-05-24 21:28:11 -0300

    Primera parte terminda

--- ChessMain.py
+++ ChessMain.py
@@ -9,3 +9,65 @@ DIMENSION = 8 #dimension of the chess board is 8x8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15 #for animations
 IMAGES = {}
+
+'''
+Initialize a global dictionary of images. This will be called exactly once in the main.
+'''
+def loadImages():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bp", "wR", "wN", "wB", "wQ", "wK", "wp"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/"+ piece + ".png"), (SQ_SIZE, SQ_SIZE))
+    #Note: we can access an image by saying IMAGE['wp']
+
+'''
+The main driver for our code. This will handle user input and updating the graphics
+'''
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    loadImages() #only do this once, before the while loop
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+'''
+Responsible for all the graphics within a current game state.
+'''
+def drawGameState(screen, gs):
+    drawBoard(screen) #draw aquares on the board
+    #add the piece hightlighting or move suggestions (later)
+    drawPieces(screen, gs.board) #draw pieces on top of those squares
+
+'''
+Draw the squares on the board. The top left square is always light.
+'''
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r+c) % 2]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+'''
+Draw the pieces on the board using the current GameState.board.
+'''
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
+
+
